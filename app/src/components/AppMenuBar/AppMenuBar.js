613029bc0b378b9f3fbcacd45179681212cab653
Julian Atkin <jatkindev@gmail.com>
2021-07-04 04:50:28 -0400

    removed extraneous buttons and changed font styling of coord display

--- app/src/components/AppMenuBar/AppMenuBar.js
+++ app/src/components/AppMenuBar/AppMenuBar.js
@@ -16,11 +16,6 @@ function AppMenuBar(props) {
                 <div className="AppMenuBar-item AppMenuBar-component">
                     <span id="coord-indicator">x: {props.coords.x.toFixed(2)}, y: {props.coords.y.toFixed(2)}, z: {props.coords.z.toFixed(2)}</span>
                 </div>
-                <div className="AppMenuBar-item AppMenuBar-component">
-                    <button id="move-mode">Move</button>
-                    <button id="rect-mode">Rect</button>
-                    <button id="home">Home</button>
-                </div>
         </div>
     );
 }
